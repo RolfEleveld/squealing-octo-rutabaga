b83607a1ab8f8d62cf115082d0a09421346d8bff
Rolf Eleveld <rolf@bajomero.com>
2015-12-28 15:32:07 +0100

    Improved deployment of Blast and MUMmer Altered renaming logic.

--- deploythemachine.sh
+++ deploythemachine.sh
@@ -53,28 +53,34 @@ sudo apt-get install r-base -y
 sudo pip install ipython-genutils
 
 ##blast
-sudo mkdir -p /mnt/work
-sudo chmod 0777 /mnt/work
-cd /mnt/work
+# using apt-get:
+sudo apt-get install ncbi-blast+ -y
+#
+#sudo mkdir -p /mnt/work
+#sudo chmod 0777 /mnt/work
+#cd /mnt/work
 # using http://www.ncbi.nlm.nih.gov/books/NBK279690/
 # look at ftp://ftp.ncbi.nih.gov/blast/executables/LATEST/ for the latest version!
-wget ftp://ftp.ncbi.nih.gov/blast/executables/blast+/2.3.0/ncbi-blast-2.3.0+-x64-linux.tar.gz
-tar xzf ncbi-blast-2.3.0+-x64-linux.tar.gz
+#wget ftp://ftp.ncbi.nih.gov/blast/executables/blast+/2.3.0/ncbi-blast-2.3.0+-x64-linux.tar.gz
+#tar xzf ncbi-blast-2.3.0+-x64-linux.tar.gz
 # make a database directory
-mkdir /mnt/work/ncbi-blast-2.3.0+/db
+#mkdir /mnt/work/ncbi-blast-2.3.0+/db
 # take the pwd and /bin and make that part of path
-export PATH=$PATH:/mnt/work/ncbi-blast-2.3.0+/bin
-export BLASTDB=$BLASTDB:/mnt/work/ncbi-blast-2.3.0+/db
+#export PATH=$PATH:/mnt/work/ncbi-blast-2.3.0+/bin
+#export BLASTDB=$BLASTDB:/mnt/work/ncbi-blast-2.3.0+/db
 # From ftp://ftp.ncbi.nih.gov/blast/documents/blast.html
 
 # MUMmer
-cd /mnt/work
-wget -O MUMmer3.23.tar.gz http://downloads.sourceforge.net/project/mummer/mummer/3.23/MUMmer3.23.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fmummer%2Ffiles%2Fmummer%2F3.23%2F&ts=1448474975&use_mirror=heanet
-tar xzf MUMmer3.23.tar.gz
-cd MUMmer3.23
-make
+# using apt-get
+sudo apt-get install MUMmer -y -q
+
+#cd /mnt/work
+#wget -O MUMmer3.23.tar.gz http://downloads.sourceforge.net/project/mummer/mummer/3.23/MUMmer3.23.tar.gz?r=http%3A%2F%2Fsourceforge.net%2Fprojects%2Fmummer%2Ffiles%2Fmummer%2F3.23%2F&ts=1448474975&use_mirror=heanet
+#tar xzf MUMmer3.23.tar.gz
+#cd MUMmer3.23
+#make
 # take the pwd and /bin and make that part of path
-export PATH=$PATH:/mnt/work/MUMmer3.23
+#export PATH=$PATH:/mnt/work/MUMmer3.23
 
 #PyANI
 cd /mnt/work
